17a9d2e0deebc3059ed1bf12b5087cf78222074c
derek williams <derek61@gmail.com>
2024-05-27 12:35:24 -0600

    updated file names

--- src/components/CatGenerator.tsx
+++ src/components/CatGenerator.tsx
@@ -1,6 +1,9 @@
 import { readFile } from 'fs/promises'
+import { RevealButton } from './reveal-button'
 
 export default async function CatGenerator() {
   const allCatNames = await readFile('./cats.txt', 'utf8')
   const cats = allCatNames.split('\n')
+  const randomCat = cats[Math.floor(Math.random() * (cats.length - 2))]
+  return <RevealButton catName={randomCat}></RevealButton>
 }
--- src/components/RevealButton.tsx
+++ src/components/RevealButton.tsx
@@ -4,8 +4,9 @@ import { useState } from 'react'
 
 export function RevealButton(props: { catName: string }) {
   const [revealed, setRevealed] = useState(false)
-
-  if (revealed) return <h1 className='text-4xl font-bold'>{props.catName}</h1>
+  if (revealed) {
+    return <h1 className='text-4xl font-bold'>{props.catName}</h1>
+  }
 
   return (
     <button className='text-2xl font-bold bg-green-700 py-2 px-4 rounded-lg' onClick={() => setRevealed(true)}>
